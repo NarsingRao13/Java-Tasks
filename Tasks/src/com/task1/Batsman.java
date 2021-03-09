f2ed0f919fcbd1a52bbd1ad1c3a5b26b8023e503
NarsingRao13 <narsingrao.k@techouts.com>
2021-03-09 12:05:35 +0530

    commit

--- Tasks/src/com/task1/Batsman.java
+++ Tasks/src/com/task1/Batsman.java
@@ -1,18 +1,19 @@
 package com.task1;
+
 import java.math.*;
 
 public class Batsman {
 	private String name;
 	private int runScored;
 	private int centuries;
-	private int	halfCenturies;
+	private int halfCenturies;
 	private int ballsFaced;
 	private int fours;
 	private int sixes;
 	private int id;
-	
-	void setData(int id,String name,int runScored,int centuries,int halfCenturies,
-			int ballsFaced,int fours,int sixes){
+
+	void setData(int id, String name, int runScored, int centuries, int halfCenturies, int ballsFaced, int fours,
+			int sixes) {
 		this.id = id;
 		this.name = name;
 		this.runScored = runScored;
@@ -22,32 +23,33 @@ public class Batsman {
 		this.fours = fours;
 		this.sixes = sixes;
 	}
-	
-	float getStrikeRate(){
-		
-		//float f = roundFloat(2343.0f, 2);
-	    //System.out.println("Rounded Float: "+f);
-		float res= (float)((runScored*100)/ballsFaced);
-		//System.out.println("StrikeRate: "+res);
+
+	float getStrikeRate() {
+
+		// float f = roundFloat(2343.0f, 2);
+		// System.out.println("Rounded Float: "+f);
+		float res = (float) ((runScored * 100) / ballsFaced);
+		// System.out.println("StrikeRate: "+res);
 		return roundFloat(res, 2);
 	}
-	
+
 	private static float roundFloat(float f, int places) {
-		 
-        BigDecimal bigDecimal = new BigDecimal(Float.toString(f));
-        bigDecimal = bigDecimal.setScale(places, RoundingMode.HALF_UP);
-        return bigDecimal.floatValue();
-    }
-	
-	int getRunsScoredInBoundaries(){
-		return ( 4 * fours + 6 * sixes );
-	}
-	
-	public Batsman(){
-		
+
+		BigDecimal bigDecimal = new BigDecimal(Float.toString(f));
+		bigDecimal = bigDecimal.setScale(places, RoundingMode.HALF_UP);
+		return bigDecimal.floatValue();
 	}
 
-	public Batsman(int id,String name, int runScored, int centuries, int halfCenturies, int ballsFaced, int fours, int sixes) {
+	int getRunsScoredInBoundaries() {
+		return (4 * fours + 6 * sixes);
+	}
+
+	public Batsman() {
+
+	}
+
+	public Batsman(int id, String name, int runScored, int centuries, int halfCenturies, int ballsFaced, int fours,
+			int sixes) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -58,7 +60,7 @@ public class Batsman {
 		this.fours = fours;
 		this.sixes = sixes;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -125,12 +127,9 @@ public class Batsman {
 
 	@Override
 	public String toString() {
-		return "Batsman [ id=" + this.id + ", name=" + this.name + ", runScored=" + this.runScored + ", centuries=" + this.centuries + ", halfCenturies="
-				+ this.halfCenturies + ", ballsFaced=" + this.ballsFaced + ", fours=" + this.fours + ", sixes=" + this.sixes
-				+ "]";
+		return "Batsman [ id=" + this.id + ", name=" + this.name + ", runScored=" + this.runScored + ", centuries="
+				+ this.centuries + ", halfCenturies=" + this.halfCenturies + ", ballsFaced=" + this.ballsFaced
+				+ ", fours=" + this.fours + ", sixes=" + this.sixes + "]";
 	}
-	
-	
-	
-	
+
 }
